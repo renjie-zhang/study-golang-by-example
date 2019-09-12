5f5693b8488e49ffd09e5279dfecad1498bf39e0
renjie.zhang <joker_zhang@aliyun.com>
2019-09-12 22:51:35 +0800

    init

---
+++ HelloWorld/Hello.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+}
+
+//先行惯例，来一个hello world
---
+++ Variable/Variable.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = "Load"
+	fmt.Println(a)
+
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	var d = true
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+
+	f := "apple"
+	fmt.Println(f)
+}
+
+/*
+输出
+Load
+1 2
+true
+0
+apple
+*/
