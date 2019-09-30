15aaf583467030610831d29fa706cb5e6a276ebe
renjie.zhang <joker_zhang@aliyun.com>
2019-09-30 23:01:42 +0800

    update

--- 01-HelloWorld/Hello.go
+++ 01-HelloWorld/Hello.go
@@ -1,9 +1,17 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: renjie.zhang
+ * @Date: 2019-09-30 17:59:35
+ * @LastEditTime: 2019-09-30 17:59:35
+ */
 package main
 
 import "fmt"
 
 func main() {
 	fmt.Println("Hello World")
+
 }
 
 //先行惯例，来一个hello world
