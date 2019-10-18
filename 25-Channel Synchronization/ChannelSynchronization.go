64a80073c18406aacd74ec3b9226458d4c8c5dfb
renjie.zhang <joker_zhang@aliyun.com>
2019-10-18 02:47:46 +0000

    update readme.md

---
+++ 25-Channel Synchronization/ChannelSynchronization.go
@@ -0,0 +1,17 @@
+package main
+import(
+	"fmt"
+	"time"
+)
+
+func main(){
+	done := make(chan bool,1)
+	go worker(done)
+	<-done
+}
+
+func worker(done chan bool){
+	fmt.Print("working...")
+	time.Sleep(time.Second)
+	fmt.Println("DONE")
+}
