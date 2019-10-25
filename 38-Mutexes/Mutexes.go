958c62256ed9c320b8cc831616eec8bfdce0d0cd
renjie.zhang <joker_zhang@aliyun.com>
2019-10-25 09:14:44 +0000

    update mutexes

--- 38-Mutexes/Mutexes.go
+++ 38-Mutexes/Mutexes.go
@@ -1,6 +1,21 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: renjie.zhang
+ * @Date: 2019-10-25 17:10:04
+ * @LastEditTime: 2019-10-25 17:13:51
+ */
 package main
 
-func main(){
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+func main() {
 	var state = make(map[int]int)
 
 	var mutex = &sync.Mutex{}
@@ -8,30 +23,29 @@ func main(){
 	var readOps uint64
 	var writeOps uint64
 
-	for r := 0;r<100;r++ {
-		go func(){
+	for r := 0; r < 100; r++ {
+		go func() {
 			total := 0
 			for {
 				key := rand.Intn(5)
 				mutex.Lock()
 				total += state[key]
-				mutex.UnLock()
-				atomic.AddUint64(&readOps,1)
-
+				mutex.Unlock()
+				atomic.AddUint64(&readOps, 1)
 				time.Sleep(time.Millisecond)
 			}
 		}()
 	}
 
-	for w := 0;w<10;w++{
-		go func(){
-			for{
+	for w := 0; w < 10; w++ {
+		go func() {
+			for {
 				key := rand.Intn(5)
-				val := read.Intn(100)
+				val := rand.Intn(100)
 				mutex.Lock()
 				state[key] = val
-				mutex.UnLock()
-				atomic.AddUint64(&writeOps,1)
+				mutex.Unlock()
+				atomic.AddUint64(&writeOps, 1)
 				time.Sleep(time.Millisecond)
 			}
 		}()
@@ -39,14 +53,13 @@ func main(){
 
 	time.Sleep(time.Second)
 
-	readOpsFinal := atomic.LoadInt64(&readOps)
-	fmt.Println("readopc:",readOpsFinal)
-	writeOpsFinal := atomic.LoadInt64(&writeOps)
-	fmt.Println("writeopc:",writeOps)
+	readOpsFinal := atomic.LoadUint64(&readOps)
+	fmt.Println("readopc:", readOpsFinal)
+	writeOpsFinal := atomic.LoadUint64(&writeOps)
+	fmt.Println("writeopc:", writeOpsFinal)
 
 	mutex.Lock()
-	fmt.Println("state:",state)
-	mutex.UnLock()
-
+	fmt.Println("state:", state)
+	mutex.Unlock()
 
 }
