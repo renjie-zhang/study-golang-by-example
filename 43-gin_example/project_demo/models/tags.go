62e2da8ade9a9ba9b1103b902bdea97e8c70ceae
renjie zhang <joker_zhang@aliyun.com>
2021-06-14 22:32:53 +0800

    update Code

--- 43-gin_example/project_demo/models/tags.go
+++ 43-gin_example/project_demo/models/tags.go
@@ -14,19 +14,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package util
+package models
 
-import (
-	"github.com/gin-gonic/gin"
-	"github.com/unknwon/com"
-	"project_demo/pkg/setting"
-)
+type Tag struct {
+	Model
 
-func GetPage(context *gin.Context) int {
-	result := 0
-	page, _ := com.StrTo(context.Query("page")).Int()
-	if page > 0 {
-		result = (page - 1) * setting.PageSize
-	}
-	return result
+	Name string `json:"name"`
+	CreatedBy string `json:"created_by"`
+	ModifiedBy string `json:"modified_by"`
+	State int `json:"state"`
 }
