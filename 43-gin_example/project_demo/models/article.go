0f94c7f6106b890f1fed5f1d240dcb2ac699b3e8
renjie zhang <joker_zhang@aliyun.com>
2021-06-20 22:15:50 +0800

    Create article.go

---
+++ 43-gin_example/project_demo/models/article.go
@@ -0,0 +1,43 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package models
+
+import (
+	"github.com/jinzhu/gorm"
+	"time"
+)
+
+type Article struct {
+	Model
+	TagID int `json:"tag_id" gorm:"index"`
+	Tag Tag `json:"tag"`
+
+	Title string `json:"title"`
+	Desc string `json:"desc"`
+	Content string `json:"content"`
+	CreatedBy string `json:"created_by"`
+	ModifiedBy string `json:"modified_by"`
+	State int `json:"state"`
+}
+
+func (article *Article) BeforeCreate(scope *gorm.Scope) error{
+	return scope.SetColumn("CreatedOn",time.Now().Unix())
+}
+
+func (article *Article) BeforeUpdate(scope *gorm.Scope) error{
+	return scope.SetColumn("ModifiedOn", time.Now().Unix())
+}
